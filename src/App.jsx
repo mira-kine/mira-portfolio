48a30b6d7757c3078e48261e8c57f3b2601106b4
mira-kine <mirak0419@gmail.com>
2022-06-11 11:56:18 -0700

    building out Home View, imported necessary packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './views/Home/Home'
 import './App.css'; /* Global CSS */
 import styles from './App.module.css'; /* CSS Modules */
 
 export default function App() {
   return (
-    <h1
-      className={`
-      bg-green-400
-        text-3xl
-        text-center
-      text-white
-        font-bold
-        p-10
-        w-1/2
-        mx-auto
-        mt-10
-        ${styles.myCustomCssClass}
-      `}
-    >
-      Hello, World!
-    </h1>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
