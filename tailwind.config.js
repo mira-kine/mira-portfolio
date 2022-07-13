be398dbc4942dfc1ede46703ee027d51ea7ccfbb
mira-kine <mirak0419@gmail.com>
2022-07-13 12:22:58 -0700

    project base design template done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        landing: "url('../public/assets/temp-sky.jpg')",
-        sunflower: "url('../public/assets/sunflower-pc.jpg')",
-        headshot: "url('../public/assets/headshot_mira.jpg')",
+        landing: "url('/public/assets/temp-sky.jpg')",
+        sunflower: "url('/public/assets/sunflower-pc.jpg')",
+        headshot: "url('/public/assets/headshot_mira.jpg')",
+        goodjob: "url('/public/assets/goodjob-gif.gif')",
+      },
+      padding: {
+        '1/3': '33.33333%',
+        '2/3': '66.66667%',
       },
     },
   },
