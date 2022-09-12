69b090060f79527b83a1d211ed86d8a513b28de8
mira-kine <mirak0419@gmail.com>
2022-09-12 16:13:19 -0700

    added chakra provider in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import './style.css';
 
 render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
