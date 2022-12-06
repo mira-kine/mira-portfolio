40c714e2fdbfe1484f1c9a4a095ec0f05ff26d8f
mira-kine <mirak0419@gmail.com>
2022-12-06 15:57:41 -0800

    added anchor tags to social media on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <div class="border-b-2 shadow-md border-gray-100 pb-5">
       <div class="max-w-7xl mx-auto my-3 px-3 sm:px-6 ">
@@ -8,7 +11,11 @@ export default function NavBar() {
           <div class="flex justify-around">
             <div class="flex justify-end ">
               <div class="flex whitespace-nowrap pt-1  mx-3justify-center space-x-1 ">
-                <a href="#" class="text-gray-900 hover:text-gray-500">
+                <a
+                  href="https://www.instagram.com/mirakinebuchi"
+                  target="_blank"
+                  class="text-gray-900 hover:text-gray-500"
+                >
                   <span class="sr-only">Instagram</span>
                   <svg
                     class="w-6 h-6"
@@ -23,7 +30,11 @@ export default function NavBar() {
                     ></path>
                   </svg>
                 </a>
-                <a href="#" class="text-gray-900 hover:text-gray-500">
+                <a
+                  href="https://www.twitter.com/mirakine_"
+                  target="_blank"
+                  class="text-gray-900 hover:text-gray-500"
+                >
                   <span class="sr-only">Twitter</span>
                   <svg
                     class="w-6 h-6"
@@ -34,7 +45,11 @@ export default function NavBar() {
                     <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
                   </svg>
                 </a>
-                <a href="#" class="text-gray-900 hover:text-gray-500">
+                <a
+                  href="https://www.github.com/mira-kine"
+                  target="_blank"
+                  class="text-gray-900 hover:text-gray-500"
+                >
                   <span class="sr-only">GitHub</span>
                   <svg
                     class="w-6 h-6"
@@ -61,7 +76,8 @@ export default function NavBar() {
                 class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-900 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                 aria-expanded="false"
               >
-                <span class="sr-only">Open menu</span>
+                <span class="sr-only text-charcoal">Open menu</span>
+                <span class="sr-only text-charcoal">Menu</span>
                 <svg
                   class="h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
