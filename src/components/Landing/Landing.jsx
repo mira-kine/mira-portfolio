091ad72097175fa96b1e72a70d09d1850d3768ac
mira-kine <mirak0419@gmail.com>
2022-12-08 12:16:17 -0800

    centering landing page and adding responsive design

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,14 +4,22 @@ import NavBar from '../NavBar/NavBar';
 
 export default function Landing() {
   return (
-    <div className="snap-center flex flex-col h-screen">
+    <div
+      aria-label="main-container"
+      className="snap-center flex flex-col h-screen"
+    >
       <NavBar />
       {/* add cute hellow gif here */}
-      <div class="bg-pink h-full w-full">
-        <h1 className="text-9xl tracking-wide text-white">Mira Kinebuchi</h1>
-        <h1 className="text-6xl tracking-wide text-white">
-          Software Developer
-        </h1>
+      <div
+        aria-label="title-container"
+        className="bg-pink h-full w-full flex flex-col justify-center content-center items-center "
+      >
+        <div aria-label="name-container" className="bg-charcoal py-8 px-8">
+          <h1 className="text-9xl tracking-wide text-white">Mira Kinebuchi</h1>
+          <h1 className="text-6xl tracking-wide text-white">
+            Software Developer
+          </h1>
+        </div>
       </div>
     </div>
   );
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 export default function Projects() {
   return (
-    <div id="main-container" className="snap-center flex h-screen w-screen">
+    <div
+      aria-label="main-container"
+      className="snap-center flex h-screen w-screen"
+    >
       <div
-        id="left-screen-container"
+        aria-label="left-screen-container"
         className="flex flex-col py-5 px-5 w-2/5 border border-solid border-charcoal border-2
         "
       >
         <div
-          id="left-text-container"
+          aria-label="left-text-container"
           className="border border-solid border-charcoal border-2"
         >
           <p>
-            I am a full stack software develodiver who cares about people and
+            I am a full stack software developer who cares about people and
             their stories. My background in mental health & biopsychology helps
             me stay grounded in human connection and user experience while
             pursuing accessibility, equity and soulfulness in the digital world.
@@ -21,11 +24,11 @@ export default function Projects() {
         </div>
       </div>
       <div
-        id="right-screen-container"
+        aria-label="right-screen-container"
         className="flex py-5 px-5 bg-pink w-3/5 border border-solid border-charcoal border-2"
       >
         <div
-          id="right-timeline-container"
+          aria-label="right-timeline-container"
           className="border border-solid border-charcoal border-2 h-1/4 md:min-w-fit w-full"
         >
           Right container for projects timeline
