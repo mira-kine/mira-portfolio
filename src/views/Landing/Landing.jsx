b822a9363d6a7271b42c977113e8a446ce84deab
mira-kine <mirak0419@gmail.com>
2023-03-09 20:00:08 -0800

    responsive design about me next

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -4,7 +4,7 @@ export default function Landing() {
   return (
     <div
       aria-label="main-container"
-      className="snap-center flex flex-col h-3/5 w-full"
+      className="snap-center flex flex-col h-full w-full"
       name="home"
     >
       {/* add cute hellow gif here */}
@@ -14,12 +14,12 @@ export default function Landing() {
       >
         <div
           aria-label="name-container"
-          className="bg-white opacity-40 py-8 px-8 z-10 w-2/3 text-center"
+          className="flex flex-col justify-center bg-white opacity-40 py-8 px-8 z-10 w-full h-1/3 sm:text-center"
         >
-          <h1 className="text-4xl sm:text-6xl lg:text-8xl tracking-wide text-charcoal z-20">
+          <h1 className="text-7xl sm:text-8xl lg:text-9xl tracking-wide text-charcoal z-20">
             Mira Kinebuchi
           </h1>
-          <h1 className="text-2xl sm:text-4xl lg:text-6xl tracking-wide text-charcoal z-20">
+          <h1 className="text-3xl sm:text-4xl lg:text-6xl tracking-wide text-charcoal z-20">
             Software Developer
           </h1>
         </div>
