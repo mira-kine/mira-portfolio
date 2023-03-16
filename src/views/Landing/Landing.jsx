a2d9cc76e84774edf384793e50dbb0149575f835
mira-kine <mirak0419@gmail.com>
2023-03-16 15:49:07 -0700

    changing layout of about me to make more like me

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -14,14 +14,11 @@ export default function Landing() {
       >
         <div
           aria-label="name-container"
-          className="flex flex-col justify-center bg-white opacity-40 py-8 px-8 z-10 w-full h-1/3 sm:text-center"
+          className="flex flex-col justify-center bg-white opacity-40 py-8 px-8 z-10 w-full md:w-5/6 h-1/3 sm:text-center"
         >
-          <h1 className="text-7xl sm:text-8xl lg:text-9xl tracking-wide text-charcoal z-20">
-            Mira Kinebuchi
-          </h1>
-          <h1 className="text-3xl sm:text-4xl lg:text-6xl tracking-wide text-charcoal z-20">
-            Software Developer
-          </h1>
+          <h2 className="text-6xl md:text-7xl lg:text-8xl tracking-wide text-charcoal z-20">
+            Hi, glad you made it.
+          </h2>
         </div>
       </div>
     </div>
