c78cd36b58ea9aa130eeb3ac4cfc907457b6b20c
mira-kine <mirak0419@gmail.com>
2023-03-21 21:02:09 -0700

    add tech stack later

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -17,21 +17,32 @@ export default function AboutMe() {
             />
             <div className="flex flex-col justify-between p-4 leading-normal bg-charcoal md:bg-charcoal/0">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-pink dark:text-pink">
-                About Me:
+                My name is Mira -
               </h5>
               <p className="mb-3 text-charcoal-700 dark:text-white text-xl lg:text-2xl">
-                I am a full stack software developer who cares about people and
-                their stories. My background in mental health & biopsychology
-                helps me stay grounded in human connection and user experience
-                while pursuing accessibility, equity and soulfulness in the
-                digital world.
+                🌱 I am a full stack software developer who cares about people
+                and their stories. I am working to curate an accessible,
+                equitable and soulful digital world. 🌸 I also love creating and
+                learning new things - my personal projects employ a calm,
+                user-friendly interface, and my aesthetics are influenced by my
+                Japanese-Korean roots.
               </p>
             </div>
           </div>
         </div>
-        <div className="invisible collapse lg:visible lg:ml-32">
-          <h2>some info</h2>
-        </div>
+        {/* <div className="invisible collapse lg:visible lg:ml-32">
+          <h2>My tech stack includes:</h2>
+          <span>
+            <p>
+              Languages: JavaScript, HTML, CSS, Python, Dart Libraries &
+              Frameworks: React, Redux, Node.js, Tailwind CSS, jQuery, Express,
+              Flutter Testing: TDD, Jest, QUnit, Snapshot, Supertest Databases:
+              PostgreSQL, NoSQL, Firebase Tools: GitHub, VSCode, Heroku,
+              Netlify, Postman, Miro, Photoshop, Procreate, Canva, Beekeeper,
+              Pipedream In Progress: Typescript, Storybook
+            </p>
+          </span>
+        </div> */}
       </div>
     </>
   );
