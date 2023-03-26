8ecd74379e9a1f30ed3098f6b8a9799002a9c8b8
mira-kine <mirak0419@gmail.com>
2023-03-25 17:24:09 -0700

    cleaning up packages

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
 
 import './style.css';
 
 render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
