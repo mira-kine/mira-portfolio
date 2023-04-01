73daafbac3c75f5518e18d2d1dd34688d6edb05b
mira-kine <mirak0419@gmail.com>
2023-03-31 21:05:51 -0700

    adding some animations and fading scrolling

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,30 @@ module.exports = {
         'animate-[fade-in_1s_ease-in-out]',
         'animate-[slide-right_1s_ease-in-out]',
       ],
+      animation: {
+        fadeIn: 'fade-in-down 3s ease-out',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        wiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)',
+          },
+          '50%': {
+            transform: 'rotate(3deg)',
+          },
+        },
+      },
     },
     colors: {
       white: '#FBFBFB',
@@ -28,6 +52,9 @@ module.exports = {
       cyan: '#469B9E',
       matcha: '#AAB08A',
       sand: '#F0E5D8',
+    },
+    variants: {
+      animation: ['motion-safe'],
     },
   },
   plugins: [require('tw-elements/dist/plugin')],
