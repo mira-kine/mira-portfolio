3fe96ba4ac55f94775393254d4b65555b6d164df
mira-kine <mirak0419@gmail.com>
2023-11-10 18:26:30 -0800

    added new type for framer-motion to fix react18 compatibility bug

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const env = Object.entries({
 
 // eslint-disable-next-line
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.jsx',
   output: {
     filename: 'bundle.[hash].js',
